52d776c06f6a9361b79731a0aa53a5eb5d27f4cd
pcrubianoa <pcrubianoa@gmail.com>
2018-10-17 11:06:40 -0500

    TwitterAuthController, oauth flow to requesting token

---
+++ app/Http/Controllers/TwitterAuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Client as Guzzle;
+
+
+class TwitterAuthController extends Controller
+{
+    protected $client;
+
+    public function __construct(Guzzle $client)
+    {
+        $this->client = $client;
+    }
+
+    public function redirect()
+    {
+        $query = http_build_query([
+            'client_id' => '9',
+            'redirect_uri' => 'http://laravelclientpassport.test/auth/twitter/callback',
+            'response_type' => 'code',
+            'scope' => '*'
+        ]);
+
+        return redirect('http://laravelauthenticationpassport.test/oauth/authorize?' . $query);
+    }
+
+    public function callback(Request $request)
+    {
+        $response = $this->client->post('http://laravelauthenticationpassport.test/oauth/token', [
+            'form_params' => [
+                'grant_type' => 'authorization_code',
+                'client_id' => '9',
+                'client_secret' => 'm23EW9H0zxjoeVEkUP00CgGwigWHWrX8AQAfx7Tl',
+                'redirect_uri' => 'http://laravelclientpassport.test/auth/twitter/callback',
+                'code' => $request->code
+            ]
+        ]);
+
+        $response = json_decode($response->getBody());
+        
+        $request->user()->token()->delete();
+
+        $request->user()->token()->create([
+            'access_token' => $response->access_token
+        ]);
+
+        return redirect('/home');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/auth/twitter', 'TwitterAuthController@redirect');
+    Route::get('/auth/twitter/callback', 'TwitterAuthController@callback');
+    Route::get('/auth/twitter/refresh', 'TwitterAuthController@refresh');
+});
